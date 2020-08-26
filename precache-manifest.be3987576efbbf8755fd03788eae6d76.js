4005faad88dc551e3d4d6b972022ff1d5afd8ecf
Roque <roquealbertosoto@gmail.com>
2020-08-26 10:26:38 -0400

    Updates

---
+++ precache-manifest.be3987576efbbf8755fd03788eae6d76.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbead6d93deb818e70d25e065d29051f",
+    "url": "/Portfolio-Site/index.html"
+  },
+  {
+    "revision": "7c1ea0ec0d80a3482eb8",
+    "url": "/Portfolio-Site/static/css/main.5c8519d5.chunk.css"
+  },
+  {
+    "revision": "6e4ea33be6cef2b75735",
+    "url": "/Portfolio-Site/static/js/2.167d833f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-Site/static/js/2.167d833f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1ea0ec0d80a3482eb8",
+    "url": "/Portfolio-Site/static/js/main.1838882d.chunk.js"
+  },
+  {
+    "revision": "6503ad8617348aebb15d",
+    "url": "/Portfolio-Site/static/js/runtime-main.6d931b82.js"
+  },
+  {
+    "revision": "a3a46e22ec123bc72792d13093274322",
+    "url": "/Portfolio-Site/static/media/BackgroundArt.a3a46e22.svg"
+  },
+  {
+    "revision": "907bf455ec167f704558ee75af0b5252",
+    "url": "/Portfolio-Site/static/media/ClimateChangeVisualizer.907bf455.svg"
+  },
+  {
+    "revision": "a3fe23aea15522170d0312755d21d2b1",
+    "url": "/Portfolio-Site/static/media/DownArrow.a3fe23ae.svg"
+  },
+  {
+    "revision": "6d1796a9f798ced8961baf3c79f894b6",
+    "url": "/Portfolio-Site/static/media/Montserrat-Black.6d1796a9.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/Portfolio-Site/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9bc77c3bca968c7490de95d1532d0e87",
+    "url": "/Portfolio-Site/static/media/Montserrat-ExtraBold.9bc77c3b.ttf"
+  },
+  {
+    "revision": "38bc5e073a0692a4eddd8e61c821d57a",
+    "url": "/Portfolio-Site/static/media/Montserrat-ExtraLight.38bc5e07.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/Portfolio-Site/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/Portfolio-Site/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/Portfolio-Site/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/Portfolio-Site/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "0052573bbf05658a18ba557303123533",
+    "url": "/Portfolio-Site/static/media/Montserrat-Thin.0052573b.ttf"
+  },
+  {
+    "revision": "e10192be8469ee22074b08f9860478d8",
+    "url": "/Portfolio-Site/static/media/Ocamon.e10192be.svg"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/Portfolio-Site/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "5564c511a946428df6bf21970667a81f",
+    "url": "/Portfolio-Site/static/media/PMIS.5564c511.svg"
+  },
+  {
+    "revision": "6166caf770e899bde319e81b5358c789",
+    "url": "/Portfolio-Site/static/media/PhotoScanner.6166caf7.svg"
+  },
+  {
+    "revision": "3ee77689488a40de6103c1e76bb17e6f",
+    "url": "/Portfolio-Site/static/media/PortfolioSite.3ee77689.svg"
+  },
+  {
+    "revision": "2b14dc46fb9e653571e98d2268b32093",
+    "url": "/Portfolio-Site/static/media/UprightPhoneTrimDarkSmall.2b14dc46.svg"
+  },
+  {
+    "revision": "98ae737ae3f7d344498cc2526951b13b",
+    "url": "/Portfolio-Site/static/media/resume_Roque.98ae737a.pdf"
+  }
+]);
